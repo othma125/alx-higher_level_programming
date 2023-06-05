20a3fe688f4b36f9ba8dbd12b8a72300f23790bb
othmane <othma125@hotmail.com>
2023-06-05 14:55:44 +0100

    linked lists task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check code
+ * @h: pointer to head of list
+ * Return: boolean
+ */
+int check_cycle(listint_t *h)
+{
+	listint_t *slow, *fast;
+
+	if (h == NULL || h->next == NULL)
+		return (0);
+	slow = h->next;
+	fast = slow->next;
+	while (fast)
+	{
+		if (slow == fast)
+			return (1);
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+	return (0);
+}
