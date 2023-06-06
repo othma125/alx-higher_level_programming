943fa9c32fe1a1872ab465b5b69ad4561a68dea1
othmane <othma125@hotmail.com>
2023-06-06 14:50:47 +0100

    insert list task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * insert_node - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * @n: number
+ * Return: added node
+ */
+listint_t *insert_node(listint_t **h, int n)
+{
+	listint_t *new, *current = *h;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*h == NULL)
+	{
+		*h = new;
+		new->next = NULL;
+	}
+	else
+	{
+		while (current && current->next && current->next->n < n)
+			current = current->next;
+		new->next = current->next;
+		current->next = new;
+	}
+	return (new);
+}
