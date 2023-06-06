5c5da46467b205cdbbfa58c28cd039a12f78988d
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-06 13:20:15 +0100

    print_tebahpla task

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+n = ord('A') - ord('a')
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i + n)), end="")
