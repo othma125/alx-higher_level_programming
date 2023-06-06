a8244636c09fa4008c635d535e08b5bcdd0df848
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-06 13:03:21 +0100

    string format added

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print(f"{i // 10}{i % 10},", end="")
+        print("{}{},".format(i//10, i % 10), end="")
     else:
-        print(f"{i:d}")
+        print("{}".format(i))
