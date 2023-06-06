423e3c14e36a758a942788fc63f9846f3ec0672e
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-06 13:09:44 +0100

    string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,5 @@
 def uppercase(s):
     n = ord('A') - ord('a')
     for c in s:
-        print(f"{chr(n + ord(c)) if 97 <= ord(c) <= 122 else c}", end="")
+        print("{}".format(chr(n + ord(c)) if 97 <= ord(c) <= 122 else c), end="")
     print()
