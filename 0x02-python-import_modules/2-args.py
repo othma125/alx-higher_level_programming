e26264e98e2995b54020d8089118b1ddb7c97ba2
othmane <othma125@hotmail.com>
2023-06-08 18:11:59 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@ if __name__ == '__main__':
     """
     How to make a script dynamic
     """
-    import sys
-    print("{} arguments.".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    from sys import argv
+    print("{} {}{}{}".format(len(argv) - 1,
+                             "argument", "s" if len(argv) - 1 != 1 else "",
+                             ":" if len(argv) - 1 > 0 else "."))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
