d03935ab5d2c9bfafbc13564ed9ac5be82c878d9
othmane <othma125@hotmail.com>
2023-06-08 18:01:14 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-
     if len(argv) != 4:
         print("Usage:./ 100 - my_calculator.py < a > < operator > < b >")
         exit(1)
     op = argv[2]
-    if op == '+' or op == '-' or op == '*' or op == '/':
-        from calculator_1 import add, sub, mul, div
-        a = int(argv[1])
-        b = int(argv[3])
-        print("{} {} {}".format(a, op, b), end='')
-        if op == '+':
-            print(" = {}".format(add(a, b)))
-        elif op == '-':
-            print(" = {}".format(sub(a, b)))
-        elif op == '*':
-            print(" = {}".format(mul(a, b)))
-        else:
-            print(" = {}".format(div(a, b)))
-    else:
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    from calculator_1 import add, sub, mul, div
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {}".format(a, op, b), end='')
+    if op == '+':
+        print(" = {}".format(add(a, b)))
+    elif op == '-':
+        print(" = {}".format(sub(a, b)))
+    elif op == '*':
+        print(" = {}".format(mul(a, b)))
+    else:
+        print(" = {}".format(div(a, b)))
