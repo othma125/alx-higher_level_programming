0cae46feefc36ca15fb3daa17145ef15820c7f3b
othmane <othma125@hotmail.com>
2023-06-10 00:11:25 +0100

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,13 @@
+/**
+ * is_palindrome - check code
+ * @h: pointer to head of list
+ * Return: 0 or 1
+ */
+int is_palindrome(listint_t **h)
+{
+	listint_t* node = *h;
+
+	if (h == NULL || *h == NULL)
+		return (1);
+	
+}
