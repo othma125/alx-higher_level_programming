0e911c86a7f7d4219f0c9ba630ede9176b0b21f5
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-14 23:53:39 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,1 +1,51 @@
+#include <Python.h>
 
+/**
+ * print_python_bytes - check code
+ * @p: list
+ * return: none
+ */
+void print_python_bytes(PyObject *p)
+{
+  long int size;
+  long int i;
+	char *str;
+
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(p) == NULL)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	PyBytes_AsStringAndSize(p, &trying_str, &size);
+	printf("  size: %li\n", size);
+	printf("  trying string: %s\n", trying_str);
+		printf("  first %d bytes:", size >= 10 ? 10 : ++size);
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", str[i]);
+	printf("\n");
+}
+
+/**
+ * print_python_list - check code
+ * @p: list
+ * return: none
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size = ((PyVarObject *)p)->ob_size;
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+	int i;
+	const char *type;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
+	for (i = 0; i < size; i++)
+	{
+		type = (((PyListObject *)p)->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+			print_python_bytes(((PyListObject *)p)->ob_item[i]);
+	}
+}
