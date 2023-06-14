a271da9fc1482ec707cbbc67929d721790cf4572
othmane <othma125@hotmail.com>
2023-06-14 07:42:16 +0100

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,6 @@
 def weight_average(my_list=[]):
     if my_list is None or len(my_list) == 0:
         return 0
-    return sum(map(lambda x: x[0] * x[1], my_list)) / sum(map(lambda x: x[1], my_list))
+    sum1 = sum(map(lambda x: x[0] * x[1], my_list))
+    sum2 = sum(map(lambda x: x[1], my_list))
+    return sum1 / sum2
