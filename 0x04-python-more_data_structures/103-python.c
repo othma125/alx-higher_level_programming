5c8607b268722bd98fb418fce23e5acc1859a112
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-14 16:40:05 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -38,14 +38,16 @@ void print_python_list(PyObject *p)
 	long int size;
 	long int j;
 	PyObject *obj;
+	PyListObject *list;
 
 	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	printf("[*] Allocated = %ld\n", list->allocated);
 	for (j = 0; j < size; j++)
 	{
-		obj = ((PyListObject *)p)->ob_item[j];
+		obj = list->ob_item[j];
 		printf("Element %ld: %s\n", j, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
