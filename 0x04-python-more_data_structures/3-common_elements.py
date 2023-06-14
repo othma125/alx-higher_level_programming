eb3c8b7530fb2cb37fda98f57d1aae04cca797d5
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-15 00:06:32 +0100

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    c_set = set()
-    for x in set_1:
-        if x in set_2:
-            c_set.add(x)
-    return c_set
+    return set_1 & set_2
