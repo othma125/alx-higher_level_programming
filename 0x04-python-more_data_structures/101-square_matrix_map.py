779fa8c3adbc5e56edf063210a7a44adeb6dabb6
othmane <othma125@hotmail.com>
2023-06-14 14:36:00 +0100

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if not matrix:
-        return None
-    return [[x**2 for x in row] for row in matrix]
+    return list(map(lambda row: list(map(lambda value: value**2, row)), matrix))
