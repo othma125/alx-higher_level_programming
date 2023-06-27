20840b87752c3eeb293e53b016c7ca96b28a0e85
othmane <othma125@hotmail.com>
2023-06-27 06:58:51 +0100

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square file"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, n=0):
+        """class constructor"""
+        if type(n) is not int:
+            raise TypeError('size must be an integer')
+        if n < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = n
+
+    def area(self):
+        return self.__size ** 2
