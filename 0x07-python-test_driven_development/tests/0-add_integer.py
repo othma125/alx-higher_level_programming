06ceb5bb5771b87055c58e7469b0ac6fb2840165
othmane <othma125@hotmail.com>
2023-06-30 18:30:09 +0100

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/tests/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""add two integers module
+    if the a and b are not integers an exception is thrown
+    if a or b are floats only the floor is taken into consideration
+    otherwise the sum is returned
+"""
+
+
+def add_integer(a, b=98):
+    """Function to calculate the sum of two integers
+        If the numbers are not integers an exception is raised
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    if a == float('NaN') or b == float('NaN'):
+        raise ValueError('cannot convert float NaN to integer')
+    s = int(a) + int(b)
+    if s == a or s == b:
+        raise OverflowError('cannot convert float infinity to integer')
+    return s
