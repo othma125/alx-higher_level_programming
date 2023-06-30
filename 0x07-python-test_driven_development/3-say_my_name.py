23a2d732190055b02b5cf82d75b23fbd96c2c0fa
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-30 22:54:40 +0100

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""print first and last name module"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ prints first and last name
+        Arguments:
+            @first_name: str
+            @second_name: str
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+  
