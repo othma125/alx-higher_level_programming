7962257bfca1eb5e0139c6450c95ad435d634ff6
othmane <othma125@hotmail.com>
2023-07-10 16:08:05 +0100

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class"""
+    def __init__(self, size):
+        """ constructor"""
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """area methode"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """to string method"""
+        return f'[{self.__class__.__name__}] {self.__size}/{self.__size}'
