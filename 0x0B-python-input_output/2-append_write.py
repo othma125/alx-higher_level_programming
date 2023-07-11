23c21a7f08cad1b7d59d103c4170283973662d4c
othmane <othma125@hotmail.com>
2023-07-11 06:27:51 +0100

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""write_file module"""
+
+
+def append_write(filename="", text=""):
+    """
+    write txt to filename with append mode
+    :param text:
+    :type text:
+    :param filename:
+    :type filename:
+    :return:
+    :rtype:
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        print(text, file=f, end='')
+        return len(text)
