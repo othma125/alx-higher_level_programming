4250e594c507ae576bfe2d46f99802b672e06bb9
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-11 21:22:55 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,6 +9,6 @@ def load_from_json_file(filename):
     :rtype: str
     """
     with open(file=filename, mode='r') as file:
-        my_str = file.readline()
-    from json import loads
-    return loads(my_str)
+        from json import load
+        obj = load(file)
+    return obj
