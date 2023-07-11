3810d46ece955fb796b921087958727ba4c6aa10
othmane <othma125@hotmail.com>
2023-07-11 15:48:14 +0100

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""dictionary description with simple data structure"""
+
+
+def class_to_json(obj):
+    """dictionary description with simple data structure"""
+    return obj.__dict__
