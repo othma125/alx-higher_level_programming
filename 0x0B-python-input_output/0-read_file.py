6968695f4382c4a1310f43f34e2f4525e54efd3c
othmane <othma125@hotmail.com>
2023-07-11 06:14:42 +0100

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""read_file module"""
+
+
+def read_file(filename=""):
+    """
+    Read filename file and print if in stdout
+    :param filename:
+    :type filename:
+    :return:
+    :rtype:
+    """
+    with open('filename', encoding="utf-8") as f:
+        print(f.read(), end='')
