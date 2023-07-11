7617cc3f112d6a0996093d19aabc8410150f5d48
othmane <othma125@hotmail.com>
2023-07-11 14:22:52 +0100

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""pascal triangle module"""
+
+
+def pascal_triangle(n):
+    """
+    return dir(obj)
+    :param obj:
+    :type obj:
+    :return: dir(obj)
+    :rtype: list
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        triangle.append([1 for _ in range(i + 1)])
+        for j in range(len(triangle[i])):
+            try:
+                if i < 1 or j < 1:
+                    raise IndexError
+                triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1]
+            except IndexError:
+                pass
+    return triangle
