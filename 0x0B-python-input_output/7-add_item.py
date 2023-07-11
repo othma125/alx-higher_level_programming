a0b3c517479500563f25d584f46ba57704d766ac
othmane <othma125@hotmail.com>
2023-07-11 15:20:57 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,12 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file')\
         .load_from_json_file
+    from json.decoder import JSONDecodeError
     try:
         items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
+    except (FileNotFoundError, JSONDecodeError):
         items = []
     from sys import argv
-    items.extend(argv[1:])
+    for item in argv[1:]:
+        items.append(item)
     save_to_json_file(items, "add_item.json")
