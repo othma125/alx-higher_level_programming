c7516732a42370aa0162bef7530ae7875450956c
othmane <othma125@hotmail.com>
2023-07-11 15:38:52 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,16 +2,16 @@
 """Add all arguments to a Python list and save them to a file."""
 
 from sys import argv
+
 if __name__ == "__main__":
     """main function"""
-    if len(argv) >= 2:
-        save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-        load_from_json_file = __import__('6-load_from_json_file')\
-            .load_from_json_file
-        try:
-            items = load_from_json_file("add_item.json")
-        except FileNotFoundError:
-            items = []
-        for item in argv[1:]:
-            items.append(item)
-        save_to_json_file(items, "add_item.json")
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    for item in argv[1:]:
+        items.append(item)
+    save_to_json_file(items, "add_item.json")
