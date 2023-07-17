be83b21ea8cff02b4af85d29193e0dae6ca35dac
othmane <othma125@hotmail.com>
2023-07-17 17:02:42 +0100

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,7 +36,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ List to JSON string """
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return dumps(list_dictionaries)
 
