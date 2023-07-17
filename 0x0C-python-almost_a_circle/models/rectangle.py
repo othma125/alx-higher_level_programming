c3707244412c24a19ce1f1bd318424807baa5e13
othmane <othma125@hotmail.com>
2023-07-17 18:41:06 +0100

    models/base.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -116,8 +116,7 @@ class Rectangle(Base):
                 setattr(self, key, value)
         elif kwargs is not None and len(kwargs) > 0:
             for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """ returns a dictionary with properties """
