5aa98efec71d00c0a28bdab6e83e052ce08c8c70
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-11 14:36:25 +0100

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  while (i < num) {
+    console.log('X'.repeat(num) + '\n');
+    i++;
+  }
+}
