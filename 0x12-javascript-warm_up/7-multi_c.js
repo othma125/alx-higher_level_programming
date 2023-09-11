65a03346f46adefcbd89aa98942d1957274fd83e
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-11 14:32:52 +0100

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < num) {
+    console.log('C is fun');
+    i++;
+  }
+}
