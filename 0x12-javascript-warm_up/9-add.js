a5020cd6a42f5672cc5193ff66f3598cc1cbbb12
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-11 14:41:12 +0100

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function add(a, b) {
+  return a + b;
+}
+const { argv } = require('process');
+const x = parseInt(argv[2]);
+const y = parseInt(argv[3]);
+if (isNaN(x) || isNaN(y)) {
+  console.log('NaN');
+} else {
+  console.log(add(x, y));
+}
