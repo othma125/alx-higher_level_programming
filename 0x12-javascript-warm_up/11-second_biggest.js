ead9b4312f85214fa781b73bf4660e53e8aa8249
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-11 15:02:58 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,8 @@ const x = parseInt(argv[2]);
 if (isNaN(x)) {
   console.log(0);
 } else {
-  let biggest = x;
-  let secondBiggest = x;
+  let biggest = Number.NEGATIVE_INFINITY;
+  let secondBiggest = biggest;
   for (let i = 2; i < argv.length; i++) {
     if (parseInt(argv[i]) > biggest) {
       secondBiggest = biggest;
