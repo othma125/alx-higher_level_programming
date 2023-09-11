1e222fc9011d760243308be225ef936188afdbb2
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-11 13:28:44 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-const myVar = process.argv[2];
+const { argv } = require('process');
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else if (argv[3] === undefined) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
