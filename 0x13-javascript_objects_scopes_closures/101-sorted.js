0a184c9ba6dde96996db26039b28771c0d5f98cf
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-12 15:34:16 +0100

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-newDict = {};
+const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
