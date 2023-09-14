51b161ecb1bcd59d9a9a11af443771c7d3c7214b
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-14 16:26:55 +0100

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,14 +7,13 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    """ Print state starting with 'a' from the database hbtn_0e_6_usa
+    """ Print state id passed in as argument from the database hbtn_0e_6_usa
     """
     engine = create_engine(f'mysql+mysqldb://{argv[1]}'
                            f':{argv[2]}@localhost/{argv[3]}')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     with Session() as session:
-        states = session.query(State).order_by(State.id)
-        states = states.filter(State.name.like('%a%')
-        for state in states:
-            print(f"{state.id}: {state.name}")
+        state = session.query(State).order_by(State.id)
+        state = state.filter(State.name == argv[4]).first()
+        print(state.id if state else "Not found")
