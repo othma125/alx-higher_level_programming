1c8db127b083158f0756427fe749375b530a3aa5
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-14 16:39:19 +0100

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that lists all State objects from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """ add the State object “Louisiana” to the database hbtn_0e_6_usa
+    """
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}'
+                           f':{argv[2]}@localhost/{argv[3]}')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        session.query(State).filter(id == 2).update({State.name: 'New Mexico'})
+        session.commit()
