36d22be92e7724d6b018f3dcbaa249752189d38b
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-14 13:10:51 +0100

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module that contains a script that lists all states from the database
+    hbtn_0e_0_usa
+"""
+from MySQLdb import connect
+from sys import argv
+
+if __name__ == '__main__':
+    """ The code is not executed when imported
+    """
+    db = connect(host="localhost", port=3306, user=argv[1],
+                 passwd=argv[2], db=argv[3])
+    with db.cursor() as cursor:
+        query = "SELECT * FROM states ORDER BY id"
+        cursor.execute(query)
+        for row in cursor.fetchall():
+            if row[1] == argv[4]:
+                print(row)
+    db.close()
