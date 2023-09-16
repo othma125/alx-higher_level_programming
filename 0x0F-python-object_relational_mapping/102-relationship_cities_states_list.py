7076b5a027c48d22f09fff68fa32553a146b1ed6
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-16 14:33:55 +0100

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     """ lists all lists all City objects from the database hbtn_0e_101_usa
     """
     engine = create_engine(f'mysql+mysqldb://{argv[1]}'
-                           f':{argv[2]}@localhost/{argv[3]}')
+                           f':{argv[2]}@localhost/{argv[3]}',
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     with Session() as session:
