fef743e0d429121a682108fdcaf0ede1e5e4b676
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-28 14:16:02 +0100

    *sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-# A header variable X-School-User-Id must be sent with the value 98
 curl -sLf "$1" -X GET -H "X-School-User-Id: 98"
