2ea9e43aa23dcea84bfafea10e291869e6421fac
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-28 15:34:01 +0100

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sLf "$1" -X GET -H "X-School-User-Id: 98"
+curl -sH "X-School-User-Id: 98" "$1"
