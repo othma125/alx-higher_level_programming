28e1b8240398ba4d258e297cf2db053aef6be117
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-28 13:03:38 +0100

    1-body.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 # The size must be displayed in bytes
 # You have to use curl
-curl -sI "$1" | grep Content-Length | cut -d' ' -f2
+curl -sI "$1" | grep -i Content-Length | cut -d' ' -f2
