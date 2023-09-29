664b4225b077998f21861daa92d91354d530377b
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-29 12:23:07 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""Python script that fetches https://intranet.hbtn.io/status"""
+import urllib.request as rq
+
+
+if __name__ == "__main__":
+    with rq.urlopen("https://intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
