1035214a7c3fafabf6754ddc7c83a9c0d0371449
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-29 15:43:27 +0100

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id."""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits'
+    r = requests.get(url)
+    for commit in r.json()[:10]:
+        print(commit.get('sha'), commit.get('commit').get('author').get('name'))
