0efdb3df23e65b6d1661230ff52eb5d508391baa
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-29 12:51:03 +0100

    4-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """"Python script that fetches https://intranet.hbtn.io/status"""
-from urllib import request as rq
+import urllib
 
 
 if __name__ == "__main__":
-    with rq.urlopen("https://intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
