f90d28f59c859696ce8785408c670962c1643792
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-24 14:40:03 +0100

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
 const url = process.argv[2];
-const file = process.argv[3];
 const request = require('request');
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
+    const fs = require('fs');
+    const file = process.argv[3];
     fs.writeFile(file, body, 'utf8', (err, data) => {
       if (err) {
         return console.log(err);
