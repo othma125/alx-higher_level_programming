9bb1b9557701588832e1da1ea18843d3b2fe835b
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-24 14:16:41 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const id = parseInt(process.argv[2]);
-const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) {
