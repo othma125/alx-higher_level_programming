32cd668e88e20eeef63973ef76176d5cfa440c3c
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-24 15:09:10 +0100

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+const names = [];
+const printNames = (characters, i) => {
+  if (i === characters.length) {
+    return;
+  }
+  request(characters[i], (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      names.push(JSON.parse(body).name);
+      printNames(characters, i + 1);
+    }
+    if (i === characters.length - 1) {
+      names.forEach((name) => console.log(name));
+    }
+  });
+};
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    printNames(JSON.parse(body).characters, 0);
+  }
+});
