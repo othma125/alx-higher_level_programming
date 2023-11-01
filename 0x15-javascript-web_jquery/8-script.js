f8e77298052738507d72931a748b84d775bb0150
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-11-01 17:07:21 +0100

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* global $ */
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$.get(url, (data) => {
+    $.each(data.results, (i, movie) => {
+        $('UL#list_movies').append(`<li>${movie.title}</li>`);
+    }
+});
